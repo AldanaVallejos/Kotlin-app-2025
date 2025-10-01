c3c83fe5ca3338bd1e98c9b1db7ce89d8992c2f4
Santiago Saitz <santirash94@gmail.com>
2025-10-01 14:03:27 -0300

    Agregué a la toolbar el botón de 'menu' con opción desplegable, con opción disponible 'cerrar sesión' + agregado de un icono nativo

---
+++ app/src/main/java/com/example/tpgrupallllllllllll/DetalleJuegoActivity.kt
@@ -0,0 +1,64 @@
+package com.example.tpgrupallllllllllll
+
+import Juego
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetalleJuegoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalle_juego)
+
+        // Ajuste de paddings para edge-to-edge
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // TOOLBAR
+        val toolbar: Toolbar = findViewById(R.id.myToolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        // Botón atrás
+        val backButton = findViewById<ImageButton>(R.id.btn_ToolBar_Volver)
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        // Botón menú desplegable
+        val menuButton = findViewById<ImageButton>(R.id.btn_ToolBar_Menu)
+        menuButton.setOnClickListener { view ->
+            val popupMenu = PopupMenu(this, view)
+            popupMenu.menu.add("Cerrar sesión")
+            popupMenu.setOnMenuItemClickListener { item ->
+                if (item.title == "Cerrar sesión") {
+                    val prefs = getSharedPreferences("myPrefs", MODE_PRIVATE)
+                    prefs.edit().putString("user", "").apply()
+                    finishAffinity()
+                   // startActivity(Intent(this, loginActivity::class.java)) -> VOLVER A LA PANTALLA INICIAL
+                }
+                true
+            }
+            popupMenu.show()
+        }
+
+        // Mostrar datos del juego
+        val juego = intent.getSerializableExtra("juego") as Juego
+        findViewById<TextView>(R.id.tvDetalleNombre).text = juego.juego
+        findViewById<TextView>(R.id.tvDetalleFecha).text = juego.lanzamiento
+        findViewById<TextView>(R.id.tvDetallePrecio).text = juego.precio
+        findViewById<TextView>(R.id.tvDetalleGenero).text = juego.genero
+        findViewById<TextView>(R.id.tvDetalleValoracion).text = juego.valoracion
+    }
+}
