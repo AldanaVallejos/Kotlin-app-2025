ddcca0a8c9949af17f239133035077c252899113
Aldana  Vallejos <aldanavs@outlook.com>
2025-11-03 21:52:09 -0300

    Grupo04 aldana vallejos implementacion room (#14)
    * Base para posteriormente implementar el llamado a la API Rest.
    Creados:
    Layout de api rest.
    Interface Apiendpoints
    Clase ApirestActivity
    Clase PostDTO
    Objeto retrofitClient
    
    Agregados:
    implementaciones moshi y retrofit en gradle.
    
    * .
    
    * Agregadas funciones para trabajar correctamente con base de datos ROOM.
    Modificacion en Usuario Dao, Login Activity y Registro Activity.

--- app/src/main/java/com/example/tpgrupallllllllllll/UsuarioDao.kt
+++ app/src/main/java/com/example/tpgrupallllllllllll/UsuarioDao.kt
@@ -6,9 +6,13 @@ import androidx.room.Query
 @Dao
 interface UsuarioDao {
 
-    @Query( value =  "SELECT * FROM usuario_entity")
+    @Query(value = "SELECT * FROM usuario_entity")
     fun getAll(): List<Usuario>
 
     @Insert
     fun insert(usuario: Usuario)
+
+    // Implementacion de la funcion para buscar un usuario por email y password
+    @Query("SELECT * FROM usuario_entity WHERE mail = :email AND password = :password LIMIT 1")
+    fun login(email: String, password: String): Usuario?
 }
